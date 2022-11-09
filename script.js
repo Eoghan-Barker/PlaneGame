0ca5c1ecbc7b850b6f55655e508346e7fe4dc434
Eoghan-Barker <g00397072@gmit.ie>
2022-11-09 18:14:28 +0000

    movement
    Added user movement to the airplane, fixed margin in css

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // THREEJS VARIABLES
 var scene,
-    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
-    renderer, container;
+  camera,
+  fieldOfView,
+  aspectRatio,
+  nearPlane,
+  farPlane,
+  renderer,
+  container;
 
 //SCREEN VARIABLES
 var HEIGHT, WIDTH;
 
 // GAME VARIABLES
-var left, right, velocity;
+var left = 0,
+  right = 0,
+  velocity = 0,
+  airplane;
 
 //SCENE
 function createScene() {
-
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
 
@@ -25,17 +32,17 @@ function createScene() {
     aspectRatio,
     nearPlane,
     farPlane
-    );
-  scene.fog = new THREE.Fog(0xf7d9aa, 100,950);
+  );
+  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
   camera.position.x = 0;
   camera.position.z = 200;
   camera.position.y = 0;
-  camera.lookAt = 0,0,0;
+  (camera.lookAt = 0), 0, 0;
 
   renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
   renderer.shadowMap.enabled = true;
-  container = document.getElementById('world');
+  container = document.getElementById("world");
   container.appendChild(renderer.domElement);
 }
 
@@ -43,9 +50,8 @@ function createScene() {
 var ambientLight, hemisphereLight, shadowLight;
 
 function createLights() {
-
-  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
-  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+  shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
   shadowLight.position.set(150, 350, 350);
   shadowLight.castShadow = true;
   shadowLight.shadow.camera.left = -400;
@@ -62,106 +68,109 @@ function createLights() {
 }
 
 // Acts as a floor for the game
-function createSea(){
-    const planeGeometry = new THREE.BoxGeometry(1000, 10, 5000);
-    const planeMaterial = new THREE.MeshLambertMaterial({ color: "blue" });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    scene.add(plane);
-    plane.position.y = -50;
+function createSea() {
+  const planeGeometry = new THREE.BoxGeometry(1000, 10, 5000);
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: "blue" });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  scene.add(plane);
+  plane.position.y = -50;
 }
 
 // Game Objects
-class Airplane{
-    constructor(){
-        this.mesh = new THREE.Object3D();
-        // Main body of plane
-        var bodyGeometry = new THREE.BoxGeometry(30, 20, 50,);
-        var bodyMaterial = new THREE.MeshBasicMaterial({color: 'red'});
-        var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
-        this.mesh.add(body);
+class Airplane {
+  constructor() {
+    this.mesh = new THREE.Object3D();
+    // Main body of plane
+    var bodyGeometry = new THREE.BoxGeometry(30, 20, 50);
+    var bodyMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+    this.mesh.add(body);
 
-        // Side Wings
-        var wingsGeometry = new THREE.BoxGeometry(90, 5, 15);
-        var wingsMaterial = new THREE.MeshBasicMaterial({color: 'red'});
-        var wings = new THREE.Mesh(wingsGeometry, wingsMaterial);
-        this.mesh.add(wings);
+    // Side Wings
+    var wingsGeometry = new THREE.BoxGeometry(90, 5, 15);
+    var wingsMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var wings = new THREE.Mesh(wingsGeometry, wingsMaterial);
+    this.mesh.add(wings);
 
-        // Tail
-        var tailGeometry = new THREE.BoxGeometry(50, 5, 8);
-        var tailMaterial = new THREE.MeshBasicMaterial({color: 'red'});
-        var tail = new THREE.Mesh(tailGeometry, tailMaterial);
-        tail.position.set(0, 0, 55);
-        this.mesh.add(tail);
+    // Tail
+    var tailGeometry = new THREE.BoxGeometry(50, 5, 8);
+    var tailMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var tail = new THREE.Mesh(tailGeometry, tailMaterial);
+    tail.position.set(0, 0, 55);
+    this.mesh.add(tail);
 
-        // Cockpit
-        var cockpitGeometry = new THREE.SphereGeometry(15, 20, 20);
-        var cockpitMaterial = new THREE.MeshBasicMaterial({color: 'gray'});
-        var cockpit = new THREE.Mesh(cockpitGeometry, cockpitMaterial);
-        cockpit.position.set(0, 6, -5);
-        this.mesh.add(cockpit);
-    }
-
-    move(){
-
-    }
-}
-
-class Obsticle{
-
-}
-
-
-
-
-function createPlane(){
-    var airplane = new Airplane();
-    airplane.mesh.scale.set(.25,.25,.25);
-    airplane.mesh.position.x = 0;
-    airplane.mesh.position.y = -15;
-    airplane.mesh.position.z = 100;
-    scene.add(airplane.mesh);
+    // Cockpit
+    var cockpitGeometry = new THREE.SphereGeometry(15, 20, 20);
+    var cockpitMaterial = new THREE.MeshBasicMaterial({ color: "gray" });
+    var cockpit = new THREE.Mesh(cockpitGeometry, cockpitMaterial);
+    cockpit.position.set(0, 6, -5);
+    this.mesh.add(cockpit);
   }
 
-//player movement
-document.addEventListener('keydown', function (event)
-{
-    let key = event.key.toLowerCase();
+  //player movement - moves left or righ on keydown, stops on keyup
+  move() {
+    // Detect when user presses a,d, left or right key
+    // and when they lift off the key
+    document.addEventListener("keydown", function (event) {
+      let key = event.key.toLowerCase();
 
-    if (key == "a" || key == "arrowleft")
-    {
+      if (key == "a" || key == "arrowleft") {
         left = -1;
-    }
-    if (key == "d" || key == "arrowright")
-    {
+      }
+      if (key == "d" || key == "arrowright") {
         right = 1;
-    }
+      }
 
-    velocity = left + right;
-});
+      velocity = left + right;
+    });
 
-document.addEventListener('keyup', function (event)
-{
-    let key = event.key.toLowerCase();
+    document.addEventListener("keyup", function (event) {
+      let key = event.key.toLowerCase();
 
-    if (key == "a" || key == "arrowleft")
-    {
+      if (key == "a" || key == "arrowleft") {
         left = 0;
-    }
-    if (key == "d" || key == "arrowright")
-    {
+      }
+      if (key == "d" || key == "arrowright") {
         right = 0;
-    }
+      }
 
-    velocity = left + right;
-});
+      velocity = left + right;
+    });
+
+
+    // update the x position of the player
+    this.mesh.position.x += velocity;
+
+
+    // keep the player is in bounds - x = ~+/-102
+    if(this.mesh.position.x > 102){
+      this.mesh.position.x = 102;
+    }
+    else if(this.mesh.position.x < -102){
+      this.mesh.position.x = -102;
+    }
+  }
+}
+
+class Obsticle {}
+
+function createPlane() {
+  airplane = new Airplane();
+  airplane.mesh.scale.set(0.25, 0.25, 0.25);
+  airplane.mesh.position.x = 0;
+  airplane.mesh.position.y = -15;
+  airplane.mesh.position.z = 100;
+  scene.add(airplane.mesh);
+}
 
 function animate() {
   // Render the scene and camera
   renderer.render(scene, camera);
 
+  airplane.move();
+
   requestAnimationFrame(animate);
 }
-
 
 function init(event) {
   createScene();
@@ -169,6 +178,7 @@ function init(event) {
   createPlane();
   createSea();
   //createSky();
+
   animate();
 }
 
