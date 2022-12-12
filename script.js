c6c6b9549c13dc28a9a5798237e66031bc20299e
Eoghan-Barker <g00397072@gmit.ie>
2022-12-12 10:17:59 +0000

    V1.0
    Game at satisfactory level for submission

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ var scene,
   container;
 
 //SCREEN VARIABLES
-var HEIGHT, WIDTH, ambientLight, hemisphereLight, shadowLight;
+var HEIGHT, WIDTH, hemisphereLight, spotLight;
 
 // GAME VARIABLES
 var left = 0,
   right = 0,
   velocity = 0,
   score = 0,
-  finalScore = 0,
   collision = false,
   airplane;
 var walls = [];
@@ -44,14 +43,17 @@ function createScene() {
   camera.position.x = 0;
   camera.position.z = 200;
   camera.position.y = 0;
-  camera.lookAt(0,0,0);
+  camera.lookAt(0, 0, 0);
 
   // setup renderer
   // alpha true sets the backgroung to be transparent allowing the css to show
   // antialias true to smooth the edges
   renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
+  // Endable shadows
+  // Enable shaows in the renderer
   renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // to antialias the shadow
   // add the css and the renderer to the scene
   container = document.getElementById("world");
   container.appendChild(renderer.domElement);
@@ -59,25 +61,19 @@ function createScene() {
 
 // LIGHTS
 function createLights() {
+  // HemisphereLight to add a more realistic brightness
   hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
-  shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
-  shadowLight.position.set(150, 350, 350);
-  shadowLight.castShadow = true;
-  shadowLight.shadow.camera.left = -400;
-  shadowLight.shadow.camera.right = 400;
-  shadowLight.shadow.camera.top = 400;
-  shadowLight.shadow.camera.bottom = -400;
-  shadowLight.shadow.camera.near = 1;
-  shadowLight.shadow.camera.far = 1000;
-  shadowLight.shadow.mapSize.width = 2048;
-  shadowLight.shadow.mapSize.height = 2048;
-
-
-  var helper = new THREE.CameraHelper(shadowLight.shadow.camera);
-      scene.add(helper);
-
   scene.add(hemisphereLight);
-  scene.add(shadowLight);
+
+  // Spotlight from behind the airplane to cast shadows and improve lighting
+  spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(0, 2000, 500);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+
+  // Light direction helper for tinkering with the lighting
+  const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+  //scene.add( spotLightHelper );
 }
 
 // Acts as a floor for the game
@@ -87,51 +83,61 @@ function createSea() {
   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.receiveShadow = true;
   scene.add(plane);
+  // move the plane down to make it look like the airplane is flying higher
   plane.position.y = -50;
 }
 
 // Game Objects
 class Airplane {
   constructor() {
+    // A mesh for each piece of the airplane
     this.mesh = new THREE.Object3D();
     // Main body of plane
     var bodyGeometry = new THREE.BoxGeometry(30, 20, 50);
-    var bodyMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var bodyMaterial = new THREE.MeshLambertMaterial({ color: "red" });
     var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
     this.mesh.add(body);
 
     // Side Wings
     var wingsGeometry = new THREE.BoxGeometry(90, 5, 15);
-    var wingsMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var wingsMaterial = new THREE.MeshLambertMaterial({ color: "red" });
     var wings = new THREE.Mesh(wingsGeometry, wingsMaterial);
     this.mesh.add(wings);
 
     // Tail
     var tailGeometry = new THREE.BoxGeometry(50, 5, 8);
-    var tailMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    var tailMaterial = new THREE.MeshLambertMaterial({ color: "red" });
     var tail = new THREE.Mesh(tailGeometry, tailMaterial);
     tail.position.set(0, 0, 55);
     this.mesh.add(tail);
 
     // Cockpit
     var cockpitGeometry = new THREE.SphereGeometry(15, 20, 20);
-    var cockpitMaterial = new THREE.MeshBasicMaterial({ color: "gray" });
+    var cockpitMaterial = new THREE.MeshLambertMaterial({ color: "gray" });
     var cockpit = new THREE.Mesh(cockpitGeometry, cockpitMaterial);
     cockpit.position.set(0, 6, -5);
     this.mesh.add(cockpit);
+
+    // Misc airplane properties
+    this.mesh.scale.set(0.25, 0.25, 0.25);
+    this.mesh.position.x = 0;
+    this.mesh.position.y = -15;
+    this.mesh.position.z = 100;
+    this.mesh.castShadow = true;
+    this.mesh.recieveShadow = true;
   }
 
-  //player movement - moves left or righ on keydown, stops on keyup
+  //player movement - moves left or right on keydown, stops on keyup
   move() {
-    // Detect when user presses a,d, left or right key
+    // Detect when user presses left or right key
     // and when they lift off the key
     document.addEventListener("keydown", function (event) {
       let key = event.key.toLowerCase();
 
-      if (key == "a" || key == "arrowleft") {
+      if (key == "arrowleft") {
         left = -1;
       }
-      if (key == "d" || key == "arrowright") {
+      if (key == "arrowright") {
         right = 1;
       }
 
@@ -171,12 +177,15 @@ class Airplane {
   }
 
   crash() {
-    finalScore = score;
+    // Stops player from controlling the airplane after a crash
     collision = true;
+    // Remove the airplane from the scene to simulate a crash
     scene.remove(airplane.mesh);
+    // Display the game over screen
     document.getElementById("gameover").style.display = "block";
   }
 
+  // These are for calculating collision
   getHitboxLeft() {
     return this.mesh.position.x - 10;
   }
@@ -190,13 +199,14 @@ class Obsticle {
   constructor() {
     this.mesh = new THREE.Object3D();
     var geometry = new THREE.BoxGeometry(10, 200, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    var material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
     var wall = new THREE.Mesh(geometry, material);
     wall.position.set(0, 0, -200);
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
     this.mesh.add(wall);
 
+    // Sets the speed of the walls coming towards the player
     this.speed = 1;
   }
 
@@ -219,12 +229,13 @@ class Obsticle {
   }
 
   collisionDetect(airplane) {
+    // get the width of the wall
     this.hitboxLeft = this.mesh.position.x - 5;
     this.hitboxRight = this.mesh.position.x + 5;
 
     // Check if z coord is the same(airplane z position won't change)
     if (this.mesh.position.z >= 300 && this.mesh.position.z <= 310) {
-      // Check if x coord collision
+      // Check if x co-ord collision(check if width of wall is inside width of airplane)
       if (
         this.hitboxLeft < airplane.getHitboxRight() &&
         this.hitboxRight > airplane.getHitboxLeft()
@@ -236,20 +247,10 @@ class Obsticle {
   }
 }
 
-function createPlane() {
-  airplane = new Airplane();
-  airplane.mesh.scale.set(0.25, 0.25, 0.25);
-  airplane.mesh.position.x = 0;
-  airplane.mesh.position.y = -15;
-  airplane.mesh.position.z = 100;
-  airplane.mesh.castShadow = true;
-  airplane.mesh.recieveShadow = true;
-  scene.add(airplane.mesh);
-}
-
 function createWalls() {
   var counter = 0;
 
+  // limit the number of obsticles to 10 and stagger when they are added to the scene
   setInterval(() => {
     if (walls.length < 10) {
       walls.push(new Obsticle());
@@ -260,35 +261,31 @@ function createWalls() {
   }, 1000);
 }
 
+// Called when the user presses space after a collision
+// Resets score, adds the airplane back to the scene and lets the player control it again
 function restart() {
   scene.add(airplane.mesh);
   score = 0;
   collision = false;
   document.getElementById("gameover").style.display = "none";
-  // delete walls
-  // for (var i = walls.length; i > 0; i--) {
-  //   scene.remove(walls[i].mesh);
-  //   walls.pop();
-  //   console.log(i);
-  //   counter = 0;
-  // }
 }
 
 function animate() {
   // Render the scene and camera
   renderer.render(scene, camera);
 
-  // If statement to stop airplane.move being called when collision is detected
+  // Only allow the player to control the airplane when there is no collision
   if (!collision) {
     airplane.move();
   }
 
+  // Update all of the obsticles
   walls.forEach((wall) => {
     wall.move();
     wall.collisionDetect(airplane);
   });
 
-  // Respawn the airplane and reset the score when space is pressed after a collision
+  // Restart the game after a collision when user presses space
   document.addEventListener("keydown", function (event) {
     let key = event.key.toLowerCase();
 
@@ -303,13 +300,16 @@ function animate() {
 function init(event) {
   createScene();
   createLights();
-  createPlane();
   createSea();
-  //createSky();
   createWalls();
+  // Create player airplane object
+  airplane = new Airplane();
+  scene.add(airplane.mesh);
 
+  // hide game over message
   document.getElementById("gameover").style.display = "none";
   animate();
 }
 
+// when the game loads in, call the init function
 window.addEventListener("load", init, false);
